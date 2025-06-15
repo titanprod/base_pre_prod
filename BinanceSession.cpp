820a06519f4faaa2ce15f625972a3a54e28825c0
titanprod <bakar.ahmedou@titanarb.co.uk>
2025-06-15 12:22:28 +0300

    Update BinanceSession.cpp

--- BinanceSession.cpp
+++ BinanceSession.cpp
@@ -1,11 +1,21 @@
 #include "BinanceSession.hpp"
 #include <iostream>
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace net = boost::asio;
+namespace ssl = net::ssl;
+using tcp = net::ip::tcp;
 
 BinanceSession::BinanceSession(boost::asio::io_context& ioc, boost::asio::ssl::context& ctx)
     : resolver_(ioc), stream_(ioc, ctx) {}
 
-void BinanceSession::connect_and_handshake(std::function<void()> on_connected) {
+void BinanceSession::connect_and_handshake(
+    std::function<void()> on_connected,
+    std::function<void(boost::system::error_code)> on_error)
+{
     on_connected_ = std::move(on_connected);
+    on_error_ = std::move(on_error);
+
     resolver_.async_resolve("testnet.binance.vision", "443",
         std::bind(&BinanceSession::on_resolve, shared_from_this(),
                   std::placeholders::_1, std::placeholders::_2));
@@ -18,8 +28,17 @@ void BinanceSession::on_resolve(boost::system::error_code ec,
         std::bind(&BinanceSession::on_connect, shared_from_this(), std::placeholders::_1));
 }
 
+
 void BinanceSession::on_connect(boost::system::error_code ec) {
     if (ec) return on_error_(ec);
+
+      const std::string host = "testnet.binance.vision";
+    if (!SSL_set_tlsext_host_name(stream_.native_handle(), host.c_str())) {
+        beast::error_code ec{static_cast<int>(::ERR_get_error()), net::error::get_ssl_category()};
+        throw beast::system_error{ec};
+    }
+
+
     stream_.async_handshake(boost::asio::ssl::stream_base::client,
         std::bind(&BinanceSession::on_handshake, shared_from_this(), std::placeholders::_1));
 }
